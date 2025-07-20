34e20059a4af732030da8c911487a9f54afcf2a0
SylviaRemington <sylviaremington@gmail.com>
2025-07-19 18:38:27 -0700

    addtl notes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,4 +2,5 @@
 
 // Basic syntax for a component
 // (can do this the same way as we declare a JS arrow function)
+// Starting with accepting nothing and returning nothing; however, we want to set it up so it returns the nav section for a Navbar component.
 const Navbar = () => {};
