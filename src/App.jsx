ebc7bcd7a31ea19fc42828c83b95ae43304acbd5
SylviaRemington <sylviaremington@gmail.com>
2025-07-19 18:05:31 -0700

    changing html lines 41-53

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,19 @@ import './App.css';
 
 const App = () => {
   return (
-    <h1>Hello world!</h1>
+    <> 
+      <nav id="top-navbar">
+        <a href="/">Home</a>
+        <a href="/about-us">About Us</a>
+        <a href="/money-pit">Investment Opportunities</a>
+        <a href="/the-fine-print">Terms of Service</a>
+      </nav>
+      <main>
+        <h1>Welcome to Hyperbo.ly</h1>
+        <h2>The future is firmly in front of us!</h2>
+        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+      </main>
+    </> 
   );
 };
 
