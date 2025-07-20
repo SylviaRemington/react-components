d71981e960ef3c52f0947a297ead6ca6e525fff5
SylviaRemington <sylviaremington@gmail.com>
2025-07-20 16:42:37 -0700

    declaring and exporting the FounderListItem component

---
+++ src/components/FounderListItem/FounderListItem.jsx
@@ -0,0 +1,11 @@
+// src/components/FounderListItem/FounderListItem.jsx
+// DECLARED THE COMPONENT & EXPORTED IT //the component FounderListItem
+const FounderListItem = () => {
+  return (
+    <li>
+      <h3>Hello, I'm a super impressive Founder!</h3>
+    </li>
+  );
+};
+
+export default FounderListItem;
