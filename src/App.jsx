6b0f03270433ffccdce2bf6f5ca0b5c60304cc55
SylviaRemington <sylviaremington@gmail.com>
2025-07-20 14:09:43 -0700

    adding nav html to Navbar.jsx from App.jsx and make sure add return to it

--- src/App.jsx
+++ src/App.jsx
@@ -39,12 +39,6 @@ import './App.css';
 const App = () => {
   return (
     <> 
-      <nav id="top-navbar">
-        <a href="/">Home</a>
-        <a href="/about-us">About Us</a>
-        <a href="/money-pit">Investment Opportunities</a>
-        <a href="/the-fine-print">Terms of Service</a>
-      </nav>
       <main>
         <h1>Welcome to Hyperbo.ly</h1>
         <h2>The future is firmly in front of us!</h2>
@@ -55,3 +49,6 @@ const App = () => {
 };
 
 export default App;
+
+
+// Splitting Nav Section Into A Component
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,4 +3,14 @@
 // Basic syntax for a component
 // (can do this the same way as we declare a JS arrow function)
 // Starting with accepting nothing and returning nothing; however, we want to set it up so it returns the nav section for a Navbar component.
-const Navbar = () => {};
+// Our Objective: To have this component to return the nav section in App.jsx... so can copy/cut this nav section from App.jsx and put it here.
+const Navbar = () => {
+    // Make sure you wrap the nav bar in a return statement, like you need to do for all JS
+    return <nav id="top-navbar">
+        <a href="/">Home</a>
+        <a href="/about-us">About Us</a>
+        <a href="/money-pit">Investment Opportunities</a>
+        <a href="/the-fine-print">Terms of Service</a>
+    </nav>
+};
+
