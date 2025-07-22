9c585f9d7e2ad5f4c39cf04d7490240ede0ed8dd
SylviaRemington <sylviaremington@gmail.com>
2025-07-22 10:28:08 -0700

    adding credentials in a dynamic way

--- src/components/FounderListItem/FounderListItem.jsx
+++ src/components/FounderListItem/FounderListItem.jsx
@@ -5,6 +5,7 @@ const FounderListItem = (props) => {
   return (
     <li>
       <h3>Hello, I'm {props.founder.name}, the {props.founder.title}!</h3>
+      <p>My credentials are...wait for it, wait for it... {props.founder.credential}.</p>
     </li>
   );
 };
